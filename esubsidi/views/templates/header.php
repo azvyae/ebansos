a54efbf4940d1c6baf00b2887ba5b0e2b3c15526
erstevn <erstevn@gmail.com>
2021-06-23 22:25:09 +0700

    Updating font

--- esubsidi/views/templates/header.php
+++ esubsidi/views/templates/header.php
@@ -8,6 +8,9 @@
   <title>E-Subsidi Sarijadi | <?= $data['judul'] ?></title>
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
 </head>
 
 <body>
