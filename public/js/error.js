86e039ffb6890a3a512d2f495540343c263d602c
erstevn <erstevn@gmail.com>
2021-07-15 13:06:18 +0700

    Added particles.js, thanks to vincentgarreau

--- public/js/error.js
+++ public/js/error.js
@@ -1,7 +1,7 @@
 /* -----------------------------------------------
 /* How to use? : Check the GitHub README
 /* ----------------------------------------------- */
-
+// s
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
 particlesJS.load('particles-js', 'particles.json', function() {
