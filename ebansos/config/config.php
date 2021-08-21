1fd77d0d4303528c1d444edbc831acb4fe3f502e
Azvya Erstevan I <erstevn@gmail.com>
2021-08-21 15:28:16 +0700

    Update config.php

--- ebansos/config/config.php
+++ ebansos/config/config.php
@@ -1,15 +1,14 @@
 <?php
 
 // BASE
-define('BASEURL', 'https://ebansos.dev');
-// define('BASEURL', 'https://localhost/ebansos/public'); USED FOR LOCAL TEST ONLY
+define('BASEURL', 'https://localhost/public');
 define('APPNAME', 'Ebansos');
 define('LOGOURL', 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Lambang_Kota_Bandung.svg');
 // DB
 define('DB_HOST', 'localhost');
-define('DB_USER', 'application_ebansos');
-define('DB_PASS', 'QRbaea6BxmWDAX7q');
-define('DB_NAME', 'ebansos_dummy');
+define('DB_USER', 'USERNAME');
+define('DB_PASS', 'PASSWORD');
+define('DB_NAME', 'DATABASE');
 // Wilayah
 define('KECAMATAN', 'Sukasari');
 define('KELURAHAN', 'Sarijadi');
